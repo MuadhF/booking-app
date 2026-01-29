dc5540e808305c8a919b5848e4a55ecb9206c839
Ahmed Muadh Faizan Mohamed <axf235@bham.ac.uk>
2026-01-29 11:49:59 +0800

    corrected image filepath

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Home, MapPin, Phone, Building2, User, LogOut, Menu, X } from 'lucide-react';
 import { playerAuthApi } from '../lib/supabase';
+import logo from '../../images/3.png';
 
 interface HeaderProps {
   currentPage: string;
@@ -42,7 +43,7 @@ export default function Header({ currentPage, onPageChange, user, onSignOut }: H
             onClick={() => onPageChange('home')}
             className="absolute left-1/2 transform -translate-x-1/2 md:static md:transform-none flex items-center space-x-2 hover:opacity-80 transition-opacity flex-shrink-0"
           >
-            <img src="../../images/3.png" alt="RivoBook Logo" className="h-36 w-auto" />
+            <img src={logo} alt="RivoBook Logo" className="h-36 w-auto" />
           </button>
 
           {/* Navigation */}
